bfbef22dd6515c838e47c7ab7d2b4004c1f81a7e
wlvstv <mike@wlvs.tv>
2021-03-17 17:33:47 -0400

    Adding the initial lines
    big thanks to https://github.com/Gurgleswamp for the initial setup!

---
+++ lines.js
@@ -0,0 +1,66 @@
+    let streamers= [
+        "Bdoubleo100 ",
+        "Canadian streamer Northernlion ",
+        "Former Big Brother contestant Dan Gheesling ",
+        "New Jersey streamer wolvesatmydoor ",
+        "Plant aficionado michaelalfox "
+    ];
+    let report= [
+        "apprehended while ",
+        `attacked by ${this.getRandom(streamers)} after `,
+        "caught ",
+        "charged with ",
+        "confesses to ",
+        `confronted by ${this.getRandom(streamers)} for `,
+        "discovered to be ",
+        "found ",
+        "hospitalized after ",
+        "reported to be ",
+        "under fire for "
+    ];
+    let items= [
+        "a custom F1 livery",
+        "a titanium white Octane skin",
+        "Babyheads",
+        "Bob's Brain",
+        "Dadcraft",
+        "Delirium",
+        "Minecraft Steve",
+        "New Jersey",
+        "ten million Strawberry Creme Oreos",
+        "the Devils",
+        "the Obsidi-inn",
+        "the Wolfpack"
+    ];
+    let verb= [
+        "being the hugest Utah Jazz fan",
+        `building ${this.getRandom(items)}`,
+        `building ${this.getRandom(items)}`,
+        `complimenting some fellas' balls`,
+        `cooking ${this.getRandom(items)}`,
+        `destroying ${this.getRandom(items)}`,
+        `dismissing ${this.getRandom(items)}`,
+        `drinking ${this.getRandom(items)}`,
+        `eating ${this.getRandom(items)}`,
+        `fighting ${this.getRandom(items)}`,
+        `hiding ${this.getRandom(items)}`,
+        `judging a book by its cover`,
+        `literally throwing`,
+        `looping one too many times`,
+        `poggin' off`,
+        `not turning up the difficulty`,
+        `seeing how many chips he can fit in his mouth`,
+        `smelling ${this.getRandom(items)}`,
+        `stealing ${this.getRandom(items)}`,
+        `tasting ${this.getRandom(items)}`,
+        `turning up the difficulty`
+    ];
+    let closing= [
+        "",
+        "Investigation ongoing.",
+        "More at 9.",
+        "More to come.",
+        "Sometimes maybe good, sometimes maybe shit. ",
+        "This is NOT poggy woggy.",
+        "This is poggy woggy."
+    ];
